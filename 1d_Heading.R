6d1f73c25a3e210e2b3606b301378e1daef59ab8
ozone96 <ozone96@gmail.com>
2014-03-12 17:05:06 -0700

    Update 1d_Heading.R
    A somewhat dramatic revision of the original code to model each petri dish as a 1d system to allow for the use of the code with video tracking data.

--- 1d_Heading.R
+++ 1d_Heading.R
@@ -1,36 +1,4 @@
-mywd = "/Users/ozone96/Desktop/Internship/Guppy data"
-myfile = "Guppy Heading Data 7.csv"
-setwd(mywd)
-data = read.csv(myfile)
-
-x11s = data$Guppy.1.u1
-y11s = data$Guppy.1.v1
-x12s = data$Guppy.1.u2
-y12s = data$Guppy.1.v2
-x21s = data$Guppy.2.u1
-y21s = data$Guppy.2.v1
-x22s = data$Guppy.2.u2
-y22s = data$Guppy.2.v2
-
-Axs = data$A.x.coord
-Ays = data$A.y.coord
-Bxs = data$B.x.coord
-Bys = data$B.y.coord
-Cxs = data$C.x.coord
-Cys = data$C.y.coord
-Dxs = data$D.x.coord
-Dys = data$D.y.coord
-
-radii1 = vector(mode = "numeric", length = 0)
-radii2 = vector(mode = "numeric", length = 0)
-headings1 = vector(mode = "numeric", length = 0)
-headings2 = vector(mode = "numeric", length = 0)
-deviations1 = vector(mode = "numeric", length = 0)
-deviations2 = vector(mode = "numeric", length = 0)
-headingDiffs = vector(mode = "numeric", length = 0)
-radiusDiffs = vector(mode = "numeric", length = 0)
-
-correct = function(x1, x2, y1, y2){
+get.angle = function(x2, y2, x1, y1){
   angle = atan((y1-y2)/(x1-x2))
   if(angle > 0 & (x1-x2)<0){
     angle = pi - angle
@@ -41,37 +9,74 @@ correct = function(x1, x2, y1, y2){
       angle = 3*pi-angle
     }
   }
+  angle = round(angle*(180/pi))
   return(angle)
 }
 
-getEquation = function(x1, x2, y1, y2){
+correct = function(angle, deltaX){
+  if(!is.na(deltaX)){
+    if(angle > 180 & deltaX > 0){
+      newAngle = angle - 180
+    }else if(angle < 180 & deltaX > 0){
+      newAngle = angle + 180
+    }else{
+      newAngle = angle
+    }
+  }else{
+    newAngle = angle
+  }
+  return(newAngle)
+}
+
+equation = function(x1, x2, y1, y2){
   m = (y1-y2)/(x1-x2)
   b = y1 - m*x1
   return(c(m,b))
 }
 
-centerxs = vector(mode = "numeric", length = 0)
-centerys = vector(mode = "numeric", length = 0)
-
-for(i in 1:length(x11s)){
-  diagonal1 = getEquation(Axs[i], Ays[i], Cxs[i], Cys[i])
-  diagonal2 = getEquation(Bxs[i], Bys[i], Dxs[i], Dys[i])
-  centerxs = append(centerxs, (diagonal2[2]-diagonal1[2])/(diagonal1[1]-diagonal2[1]))
-  centerys = append(centerys, diagonal1[1]*centerxs+diagonal1[2])
-  radii1 = append(radii1, correct(centerxs[i], centerys[i], x11s[i], y11s[i]))
-  radii2 = append(radii2, correct(centerxs[i], centerys[i], x21s[i], y21s[i]))
-  headings1 = append(headings1, correct(x12s[i], y12s[i], x11s[i], y11s[i]))
-  headings2 = append(headings2, correct(x22s[i], y22s[i], x21s[i], y21s[i]))
-  deviations1 = append(deviations1, abs(headings1[i] - radii1[i]))
-  deviations2 = append(deviations2, abs(headings2[i] - radii2[i]))
-}
-for(i in 1:length(headings1)){
-  headingDiffs = append(headingDiffs, abs(headings1[i]-headings2[i]))
-  radiusDiffAngle = abs(radii1[i]-radii2[i])
-  if(radiusDiffAngle > pi){
-    radiusDiffAngle = (2*pi)-radiusDiffAngle
+oneDimension = function(data, centerX, centerY){
+  goodRows = which(is.na(data$Flag))
+  goodData = data[goodRows,]
+  x = goodData$blobX
+  y = goodData$blobY
+  dx = goodData$deltaX
+  dy = goodData$deltaY
+  angle = goodData$blobAngle
+  goodData$radius = mapply(get.angle, x, y, MoreArgs = list(centerX, centerY))
+  guppy2Rows = which(1:length(goodData$frame) %% 2 == 0)
+  guppy2Data = goodData[guppy2Rows,]
+  guppy1Rows = guppy2Rows - 1
+  guppy1Data = goodData[guppy1Rows,]
+  guppy1Parsimonious = vector(mode = "numeric", length = 0)
+  guppy2Parsimonious = vector(mode = "numeric", length = 0)
+  angle1 = guppy1Data$blobAngle
+  angle2 = guppy2Data$blobAngle
+  deltaX1 = guppy1Data$deltaX
+  deltaX2 = guppy2Data$deltaX
+  for(i in 1:length(guppy1Rows)){
+    corrected1 = correct(angle1[i], deltaX1[i])
+    corrected2 = correct(angle2[i], deltaX2[i])
+    if(i > 1){
+      if(abs(corrected1 - guppy1Parsimonious[i-1]) < abs(angle1[i] - guppy1Parsimonious[i-1])){
+        output1 = corrected1
+      }else{
+        output1 = angle1[i]
+      }
+      if(abs(corrected2 - guppy2Parsimonious[i-1]) < abs(angle2[i] - guppy2Parsimonious[i-1])){
+        output2 = corrected2
+      }else{
+        output2 = angle2[i]
+      }
+    }else{
+     output1 = angle1[i]
+     output2 = angle2[i]
+    }
+    guppy1Parsimonious = append(guppy1Parsimonious, output1)
+    guppy2Parsimonious = append(guppy2Parsimonious, output2)
   }
-  radiusDiffs = append(radiusDiffs, radiusDiffAngle)
+  guppy1Deviance = guppy1Parsimonious - guppy1Data$radius
+  guppy2Deviance = guppy2Parsimonious - guppy2Data$radius
+  result = data.frame(guppy1Parsimonious, guppy2Parsimonious, guppy1Deviance, guppy2Deviance, guppy1Deviance-guppy2Deviance)
+  colnames(result) = c("Guppy 1 Angle", "Guppy 2 Angle", "Guppy 1 Deviance", "Guppy 2 Deviance", "Difference")
+  return(result)
 }
-output = data.frame(radii1, radii2, headings1, headings2, deviations1, deviations2, headingDiffs, radiusDiffs)
-write.csv(output, file = "1d_transform.csv");
